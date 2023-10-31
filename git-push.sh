41159c7f484179e828bd7cf7f2a441ea165fd052
Matt <mattiregojos@gmail.com>
2023-10-31 08:48:53 +0000

    Add and modify files

--- git-push.sh
+++ git-push.sh
@@ -3,7 +3,7 @@
 # https://github.com/mregojos/data-pipeline-gcp
 
 git add .
-# git config --global user.email "<EMAIL ADDRESS>"
+# git config --global user.email "<EMAIL_ADDRESS>"
 git config --global user.name "Matt"
 git commit -m "Add and modify files"
 git push
